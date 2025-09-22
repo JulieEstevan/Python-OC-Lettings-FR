370b1a2ac10c4562f99e69f775bad10265b67441
JulieEstevan <juelie.s.estevan@gmail.com>
2025-09-22 16:21:40 +0200

    linting correction in settings.py

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -116,8 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # For collecting static files in production
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] # For local static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # For collecting static files in production
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # For local static files
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Sentry configuration
